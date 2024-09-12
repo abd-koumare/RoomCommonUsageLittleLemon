61bcff402511d6b20d23b681cdb2cd5d3a53b32a
Ab <46964181+abd-koumare@users.noreply.github.com>
2024-09-12 16:55:17 +0000

    Initial commit

---
+++ app/src/main/java/com/koogin/roomcommonusagelittlelemon/Database.kt
@@ -0,0 +1,79 @@
+package com.koogin.roomcommonusagelittlelemon
+
+import android.content.Context
+import androidx.room.AutoMigration
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Update
+
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var name: String,
+    var age: Int
+)
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insert(user: User)
+
+    @Query("SELECT * FROM user_table")
+    fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM user_table WHERE id = :userId")
+    fun getUserById(userId: Int): User?
+
+    @Query("SELECT * FROM user_table WHERE name = :userName")
+    fun getUserByName(userName: String): List<User>
+
+    @Delete
+    fun delete(user: User)
+
+    @Update
+    fun update(user: User)
+
+}
+
+@Database(
+    entities = [User::class],
+    version = 1, exportSchema = false,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance =
+                    Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "room-common-usage-db"
+                    ).allowMainThreadQueries()
+                        .build()
+                        INSTANCE = instance
+                        return instance
+            }
+        }
+    }
+}
